727048f3d4f1183fd3246e4259ded6b4073b4a9a
David Pham <david.pham@ext.groupepvcp.com>
2019-07-18 10:12:38 +0200

    room components

--- src/app/pages/room/room-top-five/room-top-five.component.ts
+++ src/app/pages/room/room-top-five/room-top-five.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { RoomModel } from 'src/app/models/room.model';
 
 @Component({
   selector: 'app-room-top-five',
-  templateUrl: './room-top-five.component.html'
+  templateUrl: './room-top-five.component.html',
+  styles: []
 })
 export class RoomTopFiveComponent implements OnInit {
+
+  rooms: Array<RoomModel> = [
+    { name: 'Calliope', image: 'Calliope.jpg' },
+    { name: 'Pegase', image: 'Pegase.jpg' },
+    { name: 'Thalie', image: 'Thalie.jpg' }
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
 
-}
+} 
